0b89c594362691aca4fe70b9c9683d437b8173c2
berglindoma13 <berglindoma13@ru.is>
2016-11-30 13:14:27 +0000

    stfuf

---
+++ HelloWorld/HelloWorld.iOS/MovieController.cs
@@ -0,0 +1,109 @@
+﻿using CoreGraphics;
+using DM.MovieApi;
+using MovieSearch;
+using UIKit;
+
+namespace HelloWorld.iOS
+{
+    public class MovieController : UIViewController
+    {
+        private const int HorizontalMargin = 20;
+
+        private const int StartY = 80;
+
+        private const int StepY = 50;
+
+        private int _yCoord;
+
+        private Movies _movies;
+
+        public MovieController()
+        {
+            MovieDbFactory.RegisterSettings(new MyClass());
+            this._movies = new Movies();
+        }
+            
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            this.Title = "Movie Search";
+
+            this.View.BackgroundColor = UIColor.White;
+
+            this._yCoord = StartY;
+
+            var prompt = CreatePrompt();
+
+            var nameField = CreateNameField();
+
+            var greetingButton = CreateButton("Get movies");
+
+
+            greetingButton.TouchUpInside += async (sender, args) =>
+                {
+                    this._movies.AllMovies.Clear();
+                    nameField.ResignFirstResponder();
+                    greetingButton.Enabled = false;
+                    
+                    //create the spinner whilst finding movies
+                    var spinner = new UIActivityIndicatorView(UIActivityIndicatorViewStyle.Gray);
+                    spinner.Frame = new CGRect(HorizontalMargin, this._yCoord, this.View.Bounds.Width, 50);
+                    this.View.AddSubview(spinner);
+                    spinner.StartAnimating();
+
+                    var movieApi = MovieDbFactory.Create<DM.MovieApi.MovieDb.Movies.IApiMovieRequest>().Value;
+
+                    DM.MovieApi.ApiResponse.ApiSearchResponse<DM.MovieApi.MovieDb.Movies.MovieInfo> response = await movieApi.SearchByTitleAsync(nameField.Text);
+
+                    foreach (var i in response.Results)
+                    {
+                        this._movies.AllMovies.Add(i.Title);
+                    }
+                    
+                    this.NavigationController.PushViewController(new MovieListController(this._movies.AllMovies), true);
+                    spinner.StopAnimating();
+                    greetingButton.Enabled = true;
+
+
+                };
+
+            this.View.AddSubview(prompt);
+            this.View.AddSubview(nameField);
+            this.View.AddSubview(greetingButton);
+            
+        }
+
+        private UIButton CreateButton(string title)
+        {
+            var greetingButton = UIButton.FromType(UIButtonType.RoundedRect);
+            greetingButton.Frame = new CGRect(HorizontalMargin, this._yCoord, this.View.Bounds.Width - 2*HorizontalMargin, 50);
+            greetingButton.SetTitle(title, UIControlState.Normal);
+            this._yCoord += StepY;
+            return greetingButton;
+        }
+
+        private UITextField CreateNameField()
+        {
+            var nameField = new UITextField()
+            {
+                Frame = new CGRect(HorizontalMargin, this._yCoord, this.View.Bounds.Width - 2*HorizontalMargin, 50),
+                BorderStyle = UITextBorderStyle.RoundedRect,
+                Placeholder = "Movie title"
+            };
+            this._yCoord += StepY;
+            return nameField;
+        }
+
+        private UILabel CreatePrompt()
+        {
+            var prompt = new UILabel()
+            {
+                Frame = new CGRect(HorizontalMargin, this._yCoord, this.View.Bounds.Width, 50),
+                Text = "Enter words in movie title: "
+            };
+            this._yCoord += StepY;
+            return prompt;
+        }
+    }
+}
