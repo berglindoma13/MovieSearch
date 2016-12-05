0d4415874561da5bb0c75ab7a2e1114fd3c56311
Vilhjalmur Alex <vilhjalmuralex@gmail.com>
2016-12-05 13:04:51 +0000

    added broken shit

--- HelloWorld/HelloWorld.Droid/MainActivity.cs
+++ HelloWorld/HelloWorld.Droid/MainActivity.cs
@@ -6,30 +6,46 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Views.InputMethods;
+using DM.MovieApi;
 
-namespace MovieSearch.Droid
+namespace HelloWorld.Droid
 {
-	[Activity (Label = "HelloWorld.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+	[Activity(Label = "TMDB", Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
 
-		protected override void OnCreate (Bundle bundle)
+		protected override void OnCreate(Bundle bundle)
 		{
-			base.OnCreate (bundle);
+			base.OnCreate(bundle);
+			MovieDbFactory.RegisterSettings(new ApiConnectionClass());
 
 			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+			SetContentView(Resource.Layout.Main);
 
 			// Get our button from the layout resource,
 			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
+			var button = FindViewById<Button>(Resource.Id.searchButton);
+			var info = FindViewById<TextView>(Resource.Id.textView1);
+			var search = FindViewById<EditText>(Resource.Id.searchTextField);
+			//var movieResult = FindViewById<TextView>(Resource.Id.movieResult);
+			var movieResult = FindViewById<TextView>(2131099651);
+
+
+			button.Click += async delegate
+			{
+				var manager = (InputMethodManager)this.GetSystemService(InputMethodService);
+				manager.HideSoftInputFromWindow(search.WindowToken, 0);
+
+				var movieApi = MovieDbFactory.Create<DM.MovieApi.MovieDb.Movies.IApiMovieRequest>().Value;
+
+				DM.MovieApi.ApiResponse.ApiSearchResponse<DM.MovieApi.MovieDb.Movies.MovieInfo> response = await movieApi.SearchByTitleAsync(search.Text);
+
+				var movie = response.Results[0].Title;
+
+				movieResult.Text = movie;
+
 			};
 		}
 	}
 }
-
-
